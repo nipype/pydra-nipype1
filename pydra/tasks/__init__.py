80fbb0762f566f2a02b80d1862d51c4cd661848c
Chris Markiewicz <markiewicz@stanford.edu>
2020-07-06 16:09:09 -0400

    TEST: Remove __init__

--- pydra/tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-""" Pydra tasks
-
-The ``pydra.tasks`` namespace is reserved for collections of Tasks, to be managed and
-packaged separately.
-To create a task package, please fork the `pydra-tasks-template
-<https://github.com/nipype/pydra-tasks-template>`__.
-"""
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
